421af5f88fb5a2a9384ec9ad0766a58746237a42
Takahiro F <99631358+furubay@users.noreply.github.com>
2022-03-08 16:10:12 +0900

    URL変更

--- saizeriya1000/WebView.swift
+++ saizeriya1000/WebView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import WebKit //WebKitをインポート
  
 struct WebView: UIViewRepresentable {
-    var url: String = "https://saizeria-gacha.web.app/" //表示するWEBページのURLを指定
+    var url: String = "https://saizeriya-1000yen.marusho.io/" //表示するWEBページのURLを指定
     
     func makeUIView(context: Context) -> WKWebView{
         return WKWebView()
